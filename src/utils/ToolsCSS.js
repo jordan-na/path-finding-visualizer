5c18d87d66d3f07069a560b44792035a6ce1b60a
Jordan Na <jordster02@gmail.com>
2023-01-12 14:53:59 -0500

    Updates

--- src/utils/ToolsCSS.js
+++
@@ -1,9 +0,0 @@
-const setGlobalVariable = (variable, value) => {
-   document.documentElement.style.setProperty(variable, value);
-};
-
-const ToolsCSS = {
-   setGlobalVariable: setGlobalVariable
-};
-
-export default ToolsCSS;
