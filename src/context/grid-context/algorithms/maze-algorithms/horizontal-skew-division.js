bd50f51050b7cd845825a60e986c285ae57074eb
Jordan Na <jordster02@gmail.com>
2023-01-18 13:55:35 -0500

    Updates

--- src/context/grid-context/algorithms/maze-algorithms/horizontal-skew-division.js
+++
@@ -1,6 +0,0 @@
-import { mazeDivision } from "./helper/maze-division";
-import MazeType from "../../../../enums/MazeType";
-
-export const horizontalSkewDivisionMaze = (width, height, startX, startY, targetX, targetY) => {
-   return mazeDivision(MazeType.HORIZONTAL_SKEW_DIVISION, width, height, startX, startY, targetX, targetY);
-}
