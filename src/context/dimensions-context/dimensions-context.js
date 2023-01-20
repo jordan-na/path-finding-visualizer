63ebc8c04125a0ec7ed8f69da534c3a20c205219
Jordan Na <jordster02@gmail.com>
2023-01-19 19:57:59 -0500

    Updates

--- src/context/dimensions-context/dimensions-context.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const DimensionsContext = React.createContext({
-   width: 50,
-   height: 25,
-   setGridDimensions: (width, height) => {}
-});
-
-export default DimensionsContext;
