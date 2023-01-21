af7c9a6c4533fc55e6c0e379a486374f7a06904f
Jordan Na <jordster02@gmail.com>
2023-01-20 21:01:34 -0500

    Updates

--- src/context/grid-context/algorithms/maze-algorithms/recursive-division.js
+++
@@ -1,6 +0,0 @@
-import { mazeDivision } from "./helper/maze-division";
-import MazeType from "../../../../enums/MazeType";
-
-export const recursiveDivisionMaze = (width, height, startX, startY, targetX, targetY) => {
-   return mazeDivision(MazeType.RECURSIVE_DIVISION, width, height, startX, startY, targetX, targetY);
-}
--- src/context/grid-context/algorithms/maze-algorithms/vertical-skew-division.js
+++
@@ -1,6 +0,0 @@
-import { mazeDivision } from "./helper/maze-division";
-import MazeType from "../../../../enums/MazeType";
-
-export const verticalSkewDivisionMaze = (width, height, startX, startY, targetX, targetY) => {
-   return mazeDivision(MazeType.VERTICAL_SKEW_DIVISION, width, height, startX, startY, targetX, targetY);
-};
